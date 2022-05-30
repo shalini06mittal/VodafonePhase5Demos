0404417cd4a601bcc6860ef6e8832fee1ff4a83c
Shalini Mittal <shalini06.mittal@gmail.com>
2022-05-30 16:02:32 +0530

    Day 1 Ph1 Phase 5 testng

---
+++ TestNgDemos/src/test/java/com/vf/demos/MyTestClass2.java
@@ -0,0 +1,14 @@
+package com.vf.demos;
+
+import org.testng.annotations.Test;
+
+public class MyTestClass2 extends AbstractClassTest {
+  @Test
+  public void m3() {
+	  System.out.println("m3 in test class 2");
+  }
+  @Test
+  public void m4() {
+	  System.out.println("m4 in test class 2");
+  }
+}
